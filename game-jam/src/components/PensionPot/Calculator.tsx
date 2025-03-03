c56dc73d49645a3209536b795fbf5f5bfa4c7885
James Campbell <jamescampbellek@gmail.com>
2025-03-03 23:15:36 +0000

    Fix pension pot type

--- game-jam/src/components/PensionPot/Calculator.tsx
+++ game-jam/src/components/PensionPot/Calculator.tsx
@@ -1,12 +1,13 @@
 import { PensionPot } from "../../types/PensionPot";
 
 export const PensionPotCalculator = (pensionPot: PensionPot, value: number) => {
+  // Calculate the pension pot value based on the value passed and work out the diff from the previous value as a percentage of the overall value
+  const potValue = pensionPot.value + value;
 
-    // Calculate the pension pot value based on the value passed and work out the diff from the previous value as a percentage of the overall value
-    const potValue = pensionPot.value + value;
-
-    return {
-        value: potValue,
-        diff: (((potValue - pensionPot.value) / pensionPot.value) * 100).toFixed(2),
-    };
+  return {
+    value: potValue,
+    diff: Number(
+      (((potValue - pensionPot.value) / pensionPot.value) * 100).toFixed(2)
+    ),
+  };
 };
