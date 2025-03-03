9a812f1ca1af1cab2a9972e549fa0cb26e55a32b
James Campbell <jamescampbellek@gmail.com>
2025-03-03 18:48:02 +0000

    Remove the list of all cards on the home page

--- game-jam/src/App.tsx
+++ game-jam/src/App.tsx
@@ -2,10 +2,6 @@ import "@mantine/core/styles.css";
 import { Container, MantineProvider, Stack } from "@mantine/core";
 import { VariablesDisplay } from "./components/VariablesDisplay/VariablesDisplay";
 import { CardSelect } from "./components/CardSelect/CardSelect";
-import { Card } from "./components/Card/Card";
-import { starterCards } from "./constants/starterCards";
-import { choiceCards } from "./constants/choiceCards";
-import { eventCards } from "./constants/eventCards";
 
 function App() {
   return (
@@ -16,45 +12,6 @@ function App() {
           <CardSelect />
         </Stack>
       </Container>
-
-      <h2>Starter Cards</h2>
-      {starterCards.map((card) => (
-        <Card
-          title={card.title}
-          description={card.description}
-          icon={card.icon}
-          category={card.category}
-          effectValue={card.effectValue}
-          effect={card.effect}
-          cost={card.cost}
-        />
-      ))}
-
-      <h2>Choice Cards</h2>
-      {choiceCards.map((card) => (
-        <Card
-          title={card.title}
-          description={card.description}
-          icon={card.icon}
-          category={card.category}
-          effectValue={card.effectValue}
-          effect={card.effect}
-          cost={card.cost}
-        />
-      ))}
-
-      <h2>Event Cards</h2>
-      {eventCards.map((card) => (
-        <Card
-          title={card.title}
-          description={card.description}
-          icon={card.icon}
-          category={card.category}
-          effectValue={card.effectValue}
-          effect={card.effect}
-          cost={card.cost}
-        />
-      ))}
     </MantineProvider>
   );
 }
