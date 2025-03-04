0792b2c41e9713f3cce26434cb528e6736e4e3c4
Mark Forrester <mark.m.forrester@gmail.com>
2025-03-04 11:15:58 +0000

    Add completion descriptions.

--- game-jam/src/components/CompletionSummary/CompletionSummary.tsx
+++ game-jam/src/components/CompletionSummary/CompletionSummary.tsx
@@ -1,24 +1,16 @@
 import { SimpleGrid, Stack, Text } from "@mantine/core";
-import { IconMoodHappy, IconUmbrella } from "@tabler/icons-react";
-import { SummaryCard } from "../SummaryCard/SummaryCard";
+import { PensionSummaryCard } from "../PensionSummaryCard/PensionSummaryCard";
+import { HappinessnSummaryCard } from "../HappinessSummaryCard/HappinessSummaryCard";
 
 export const CompletionSummary = () => {
   return (
     <SimpleGrid cols={{ md: 2, sm: 1 }}>
-      <SummaryCard
-        icon={<IconUmbrella size={32} stroke={1.5} />}
-        summaryType="Pension"
+      <PensionSummaryCard
         rating={80}
-      >
-        <Text mt="md">blablabla text about pension rating</Text>
-      </SummaryCard>
-      <SummaryCard
-        icon={<IconMoodHappy size={32} stroke={1.5} />}
-        summaryType="Happiness"
+      />
+      <HappinessnSummaryCard
         rating={50}
-      >
-        <Text mt="md">blablabla text about happiness rating</Text>
-      </SummaryCard>
+      />
     </SimpleGrid>
   );
 };
---
+++ game-jam/src/components/HappinessSummaryCard/HappinessSummaryCard.tsx
@@ -0,0 +1,26 @@
+import { IconUmbrella } from "@tabler/icons-react";
+import { Text } from "@mantine/core";
+import { SummaryCard } from "../SummaryCard/SummaryCard";
+
+type HappinessnSummaryCardProps = {
+  rating: number;
+};
+
+export const HappinessnSummaryCard = ({
+  rating
+}: HappinessnSummaryCardProps) => {
+  const summaryText =
+    rating >= 80 ? "You are flourishing and experiencing genuine joy, contentment, and a sense of purpose." : 
+    rating >= 60 ? "You have sense of general satisfaction with life, but without intense joy or excitement." : 
+    "While basic needs are met you have little or no enjoyment or contentment.";
+  
+  return (
+    <SummaryCard
+      icon={<IconUmbrella size={32} stroke={1.5} />}
+      summaryType="Pension"
+      rating={rating}
+      >
+      <Text mt="md">{summaryText}</Text>
+    </SummaryCard>
+  )
+}
