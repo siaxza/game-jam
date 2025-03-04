bb2acd655f1171ee9db3ca1dabb2dc989dfb6137
James Campbell <jamescampbellek@gmail.com>
2025-03-04 08:36:52 +0000

    Add the initial completion summary screen

--- game-jam/src/App.tsx
+++ game-jam/src/App.tsx
@@ -2,15 +2,23 @@ import "@mantine/core/styles.css";
 import { Container, MantineProvider, Stack } from "@mantine/core";
 import { VariablesDisplay } from "./components/VariablesDisplay/VariablesDisplay";
 import { CardSelect } from "./components/CardSelect/CardSelect";
+import { useAppSelector } from "./state/hooks";
+import { CompletionSummary } from "./components/CompletionSummary/CompletionSummary";
 
 function App() {
+  const currentStage = useAppSelector((state) => state.game.currentStage);
+
   return (
     <MantineProvider>
       <Container mt="sm">
-        <Stack>
-          <VariablesDisplay />
-          <CardSelect />
-        </Stack>
+        {currentStage === "complete" ? (
+          <CompletionSummary />
+        ) : (
+          <Stack>
+            <VariablesDisplay />
+            <CardSelect />
+          </Stack>
+        )}
       </Container>
     </MantineProvider>
   );
