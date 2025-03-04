be2705ab35d3962701fceb8039d30f91c58e4a16
James Campbell <jamescampbellek@gmail.com>
2025-03-04 13:27:35 +0000

    Fix pension animation

--- game-jam/src/components/CardSelect/CardSelect.tsx
+++ game-jam/src/components/CardSelect/CardSelect.tsx
@@ -5,11 +5,10 @@ import { useAppDispatch, useAppSelector } from "../../state/hooks";
 import { applyEffect } from "../../state/slices/gameSlice";
 import { drawNextCards } from "../../state/slices/deckSlice";
 import useSound from "use-sound";
-import shuffle from '../../riffle.mp3';
-import woosh from '../../woosh.mp3';
+import shuffle from "../../riffle.mp3";
+import woosh from "../../woosh.mp3";
 
 import { motion } from "motion/react";
-
 
 export const CardSelect = () => {
   const dispatch = useAppDispatch();
@@ -23,83 +22,85 @@ export const CardSelect = () => {
   const [play] = useSound(shuffle);
   const [playWoosh] = useSound(woosh);
 
-  
   return (
     <>
       {currentStage === "payment" ? (
         <Grid>
           <Grid.Col span={{ base: 12, sm: 4 }} offset={{ base: 0, sm: 4 }}>
-          <motion.div    exit={{ opacity:0 }}
-whileTap={{ scale: 1.1, rotate: 4, ease: [0, 0.71, 0.2, 1.01]} }>
-            <UnstyledButton
-              onClick={() => {
-                onCardSelect(cardsInHand[0]);
-              }}
-              style={{ width: "100%" }}
+            <motion.div
+              exit={{ opacity: 0 }}
+              whileTap={{ scale: 1.1, rotate: 4, ease: [0, 0.71, 0.2, 1.01] }}
             >
-              <Card
-                title={cardsInHand[0].title}
-                description={cardsInHand[0].description}
-                icon={cardsInHand[0].icon}
-                category={cardsInHand[0].category}
-                effect={cardsInHand[0].effect}
-                cost={cardsInHand[0].cost}
-                effectValue={cardsInHand[0].effectValue}
-              />
-            </UnstyledButton>
+              <UnstyledButton
+                onClick={() => {
+                  onCardSelect(cardsInHand[0]);
+                }}
+                style={{ width: "100%" }}
+              >
+                <Card
+                  title={cardsInHand[0].title}
+                  description={cardsInHand[0].description}
+                  icon={cardsInHand[0].icon}
+                  category={cardsInHand[0].category}
+                  effect={cardsInHand[0].effect}
+                  cost={cardsInHand[0].cost}
+                  effectValue={cardsInHand[0].effectValue}
+                />
+              </UnstyledButton>
             </motion.div>
           </Grid.Col>
         </Grid>
       ) : currentStage === "event" ? (
         <Grid>
           <Grid.Col span={{ base: 12, sm: 4 }} offset={{ base: 0, sm: 4 }}>
-          <motion.div whileTap={{ scale: 1.1, rotate: 5, ease: [0, 0.71, 0.2, 1.01], }}>
-            <UnstyledButton
-              onClick={() => {
-                onCardSelect(cardsInHand[0]);
-                play();
-              }}
-              style={{ height: "100%", width: "100%" }}
+            <motion.div
+              whileTap={{ scale: 1.1, rotate: 5, ease: [0, 0.71, 0.2, 1.01] }}
             >
-              <Card
-                title={cardsInHand[0].title}
-                description={cardsInHand[0].description}
-                icon={cardsInHand[0].icon}
-                category={cardsInHand[0].category}
-                effect={cardsInHand[0].effect}
-                cost={cardsInHand[0].cost}
-                effectValue={cardsInHand[0].effectValue}
-              />
-            </UnstyledButton>
+              <UnstyledButton
+                onClick={() => {
+                  onCardSelect(cardsInHand[0]);
+                  play();
+                }}
+                style={{ height: "100%", width: "100%" }}
+              >
+                <Card
+                  title={cardsInHand[0].title}
+                  description={cardsInHand[0].description}
+                  icon={cardsInHand[0].icon}
+                  category={cardsInHand[0].category}
+                  effect={cardsInHand[0].effect}
+                  cost={cardsInHand[0].cost}
+                  effectValue={cardsInHand[0].effectValue}
+                />
+              </UnstyledButton>
             </motion.div>
-
           </Grid.Col>
         </Grid>
       ) : (
         <SimpleGrid cols={{ base: 1, sm: 3 }} style={{ alignItems: "stretch" }}>
           {cardsInHand.map((card) => (
-            <motion.div whileTap={{ scale: 1.1, rotate: 4, ease: [0, 0.71, 0.2, 1.01] }}>
-
-            <UnstyledButton
-              key={card.title}
-              onClick={() => {
-                onCardSelect(card);
-                playWoosh();
-              }}
-              style={{ height: "100%" }}
+            <motion.div
+              whileTap={{ scale: 1.1, rotate: 4, ease: [0, 0.71, 0.2, 1.01] }}
             >
-              <Card
-                title={card.title}
-                description={card.description}
-                icon={card.icon}
-                category={card.category}
-                effect={card.effect}
-                cost={card.cost}
-                effectValue={card.effectValue}
-              />
-            </UnstyledButton>
+              <UnstyledButton
+                key={card.title}
+                onClick={() => {
+                  onCardSelect(card);
+                  playWoosh();
+                }}
+                style={{ height: "100%" }}
+              >
+                <Card
+                  title={card.title}
+                  description={card.description}
+                  icon={card.icon}
+                  category={card.category}
+                  effect={card.effect}
+                  cost={card.cost}
+                  effectValue={card.effectValue}
+                />
+              </UnstyledButton>
             </motion.div>
-
           ))}
         </SimpleGrid>
       )}
